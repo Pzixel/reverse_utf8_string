84e1d43dd2f88f2130d10c4c2c0bfd2d99280f57
Psilon <pzixel@gmail.com>
2018-10-05 17:38:52 +0300

    fix utf issues

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,42 @@ fn reverse_string(s: &str) -> String {
 
     let bytes = s.as_bytes();
     let mut result = vec![0; bytes.len()];
-    let mut i = 0;
-    let mut j = bytes.len() - 1;
-    while i <= j {
-        let i_end = loop {
-            let mut value = i;
-            if is_first_byte(bytes[value + 1]) {
-                break value
+    let mut i_src = 0;
+    let mut j_src = bytes.len() - 1;
+    let mut i_dst = i_src;
+    let mut j_dst = j_src;
+
+    while i_src <= j_src {
+        let i_end = {
+            let mut value = i_src;
+            loop {
+                if is_first_byte(bytes[value + 1]) {
+                    break value
+                }
+                value += 1;
             }
-            value += 1;
         };
 
-        let j_start = loop {
-            let mut value = j;
-            if is_first_byte(bytes[value - 1]) {
-                break value
+        let j_start = {
+            let mut value = j_src;
+            loop {
+                if is_first_byte(bytes[value]) {
+                    break value
+                }
+                value -= 1;
             }
-            value -= 1;
         };
 
-        let first_char_length = i_end - i + 1;
-        let last_char_length = j - j_start + 1;
+        let first_char_length = i_end - i_src + 1;
+        let last_char_length = j_src - j_start + 1;
 
-        result[i..i+last_char_length].copy_from_slice(&bytes[j_start..j_start + last_char_length]);
-        result[j_start..j_start+first_char_length].copy_from_slice(&bytes[i..i+first_char_length]);
+        result[i_dst..][..last_char_length].copy_from_slice(&bytes[j_start..][..last_char_length]);
+        result[j_dst - first_char_length + 1..][..first_char_length].copy_from_slice(&bytes[i_src..][..first_char_length]);
 
-        i = i_end + 1;
-        j = j_start - 1;
+        i_src = i_end + 1;
+        j_src = j_start - 1;
+        i_dst += last_char_length;
+        j_dst -= first_char_length;
     }
     String::from_utf8(result).unwrap()
 }
@@ -43,6 +52,23 @@ fn is_first_byte(value: u8) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    macro_rules! test_char {
+        ($c:expr, $test:ident) => {
+            #[test]
+            fn $test() {
+                let bytes = $c.as_bytes();
+                assert!(is_first_byte(bytes[0]), "First byte");
+                for i in 1..bytes.len() {
+                    assert!(!is_first_byte(bytes[i]), format!("Byte #{}", i));
+                }
+            }
+        }
+    }
+
+    test_char!("😮", four_byte);
+    test_char!("€", three_byte);
+    test_char!("¢", two_byte);
 
     #[test]
     fn first_byte() {
@@ -65,5 +91,10 @@ mod tests {
     #[test]
     fn reverse_empty() {
         assert_eq!("", &reverse_string(""));
+    }
+
+    #[test]
+    fn reverse_unicode() {
+        assert_eq!("€$¢😮", &reverse_string("😮¢$€"));
     }
 }
