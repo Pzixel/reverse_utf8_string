f5a8317450b51006ec897a80359716713dc2aa45
Psilon <pzixel@gmail.com>
2018-10-05 16:31:51 +0300

    fix several scenarios

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 fn reverse_string(s: &str) -> String {
+    if s == "" {
+        return "".into();
+    }
+
     let bytes = s.as_bytes();
     let mut result = vec![0; bytes.len()];
     let mut i = 0;
     let mut j = bytes.len() - 1;
-    while i < j {
+    while i <= j {
         let i_end = loop {
             let mut value = i;
-            if is_not_first_byte(bytes[value + 1]) {
+            if is_first_byte(bytes[value + 1]) {
                 break value
             }
             value += 1;
@@ -14,7 +18,7 @@ fn reverse_string(s: &str) -> String {
 
         let j_start = loop {
             let mut value = j;
-            if is_not_first_byte(bytes[value - 1]) {
+            if is_first_byte(bytes[value - 1]) {
                 break value
             }
             value -= 1;
@@ -23,8 +27,8 @@ fn reverse_string(s: &str) -> String {
         let first_char_length = i_end - i + 1;
         let last_char_length = j - j_start + 1;
 
-        result[i..last_char_length].copy_from_slice(&bytes[j..last_char_length]);
-        result[j_start..first_char_length].copy_from_slice(&bytes[i..first_char_length]);
+        result[i..i+last_char_length].copy_from_slice(&bytes[j_start..j_start + last_char_length]);
+        result[j_start..j_start+first_char_length].copy_from_slice(&bytes[i..i+first_char_length]);
 
         i = i_end + 1;
         j = j_start - 1;
@@ -32,8 +36,8 @@ fn reverse_string(s: &str) -> String {
     String::from_utf8(result).unwrap()
 }
 
-fn is_not_first_byte(value: u8) -> bool {
-    (value & 0b10000000) == 0b10000000 && (value & 0b01000000) == 0
+fn is_first_byte(value: u8) -> bool {
+    !((value & 0b10000000) == 0b10000000 && (value & 0b01000000) == 0)
 }
 
 #[cfg(test)]
@@ -42,14 +46,24 @@ mod tests {
 
     #[test]
     fn first_byte() {
-        assert!(is_not_first_byte(0b10101010));
-        assert!(is_not_first_byte(0b10111110));
-        assert!(!is_not_first_byte(0b00111110));
-        assert!(!is_not_first_byte(0b01111110));
+        assert!(!is_first_byte(0b10101010));
+        assert!(!is_first_byte(0b10111110));
+        assert!(is_first_byte(0b00111110));
+        assert!(is_first_byte(0b01111110));
     }
 
     #[test]
-    fn reverse() {
+    fn reverse_even() {
+        assert_eq!("abcdef", &reverse_string("fedcba"));
+    }
+
+    #[test]
+    fn reverse_odd() {
         assert_eq!("olleh", &reverse_string("hello"));
+    }
+
+    #[test]
+    fn reverse_empty() {
+        assert_eq!("", &reverse_string(""));
     }
 }
